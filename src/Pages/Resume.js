a8c92c1c0cf308c957283bf74f22c5beed5f1ff9
zanemaklin <zmaklin@gmail.com>
2022-05-28 00:59:42 -0400

    Front End Resume Download

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Resume = () => {
   return (
+    <div>
     <h1>
         Resume
     </h1>
+    <div className='content_border'>
+    <button className='File-Download'>
+      <Link to="/Data/Zane_Maklin_Resume.pdf" target="Resume" download>Download Resume</Link>
+    </button>
+    </div>
+    </div>
   )
 }
 
